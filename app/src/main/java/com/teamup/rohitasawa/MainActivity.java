2b8459d34295148712e8a5316b36984725e15389
okcoding21 <rohit.asawa21@gmail.com>
2020-07-26 16:28:48 +0530

    c

--- app/src/main/java/com/teamup/rohitasawa/MainActivity.java
+++ app/src/main/java/com/teamup/rohitasawa/MainActivity.java
@@ -2,12 +2,30 @@ package com.teamup.rohitasawa;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.SyncStateContract;
 import android.widget.Button;
 
+import com.teamup.rohitasawa_library.RohitDialog;
 import com.teamup.rohitasawa_library.RohitInternetCheck;
+import com.teamup.rohitasawa_library.RohitNotification;
+import com.teamup.rohitasawa_library.RohitPleaseWait;
 import com.teamup.rohitasawa_library.RohitToast;
 
 public class MainActivity extends AppCompatActivity {
